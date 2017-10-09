03612330389cac022a001c9f7ecd0a4a31265c4c
lsouth <lsouth269@gmail.com>
2017-10-09 17:26:15 -0600

    Create JUnit test cases for game classes

---
+++ src/edu/colostate/cs/cs414/andyetitcompiles/p3/JungleTileTest.java
@@ -0,0 +1,14 @@
+package edu.colostate.cs.cs414.andyetitcompiles.p3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JungleTileTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
