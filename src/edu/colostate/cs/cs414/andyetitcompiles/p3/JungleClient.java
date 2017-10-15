8897d1fae62fa48ccdc24c3235c9999f80f970b0
IsaacIzy <izy@rams.colostate.edu>
2017-10-15 16:28:54 -0600

    Create initial client files, repackage test and production code

---
+++ src/edu/colostate/cs/cs414/andyetitcompiles/p3/JungleClient.java
@@ -0,0 +1,39 @@
+package edu.colostate.cs.cs414.andyetitcompiles.p3;
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+
+public class JungleClient {
+	Client kryoClient;
+	boolean loggedIn;
+	User clientUser;
+	
+	public JungleClient() {
+		
+	}
+	
+	public void login(String email, String password) {
+		
+	}
+	
+	public void register(String email, String nickname, String password) {
+		
+	}
+	
+	public void unregister() {
+		
+	}
+	
+	public User findUser(String nickname) {
+		return null;
+	}
+	
+	public void invite(User otherUser) {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/edu/colostate/cs/cs414/andyetitcompiles/p3/Response.java
@@ -0,0 +1,11 @@
+package edu.colostate.cs.cs414.andyetitcompiles.p3;
+
+/* 
+ * Response will be the object we are sending on the client and server to represent responses
+ * to any received Request objects. It will have a type enum that represents the different types
+ * of responses, as well as a content Object that corresponds to the type. This could be an invitation
+ * acceptance/rejection, game state update, user query result, etc.
+ */
+public class Response {
+
+}
