2f467eb7152a6c5068ead69dba6047cb13df7b8b
lsouth <lsouth269@gmail.com>
2017-10-19 13:42:04 -0600

    Add dens, traps, rivers to board setup

--- src/edu/colostate/cs/cs414/andyetitcompiles/p3/common/JungleBoard.java
+++ src/edu/colostate/cs/cs414/andyetitcompiles/p3/common/JungleBoard.java
@@ -1,5 +1,45 @@
 package edu.colostate.cs.cs414.andyetitcompiles.p3.common;
 
 public class JungleBoard {
+	private final int ROWS = 9;
+	private final int COLS = 7;
+	
+	protected JungleTile[][] tiles;
 
+	public JungleBoard(){
+		tiles = new JungleTile[ROWS][COLS];
+		TileType type;
+		for(int i = 0; i < ROWS; i++){
+			for(int j = 0; j < COLS; j++){
+				type = TileType.NORMAL;
+				if((i == 0 && j == 3) || (i == 8 && j == 3))
+					type = TileType.DEN;
+				if((i <= 5 && i >= 3) && (j == 1 || j == 2 || j == 4 || j== 5))
+					type = TileType.RIVER;
+				if((i == 0 || i == 8) && (j == 2 || j == 4))
+					type = TileType.TRAP;
+				if((i == 1 && j == 3) || (i == 7 && j == 3))
+					type = TileType.TRAP;
+				tiles[i][j] = new JungleTile(type);
+			}
+		}
+	}
+	
+	public void printBoard(){
+		for(int i = 0; i < ROWS; i++){
+			for(int j = 0; j < COLS; j++){
+				System.out.print(tiles[i][j].getType() + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public int getRows() {
+		return ROWS;
+	}
+
+	public int getCols() {
+		return COLS;
+	}
+	
 }
--- src/edu/colostate/cs/cs414/andyetitcompiles/p3/common/JungleTile.java
+++ src/edu/colostate/cs/cs414/andyetitcompiles/p3/common/JungleTile.java
@@ -1,5 +1,18 @@
 package edu.colostate.cs.cs414.andyetitcompiles.p3.common;
 
 public class JungleTile {
+	protected TileType type;
+	
+	public JungleTile(TileType type){
+		this.type = type;
+	}
 
+	public TileType getType() {
+		return type;
+	}
+
+	public void setType(TileType type) {
+		this.type = type;
+	}
+	
 }
