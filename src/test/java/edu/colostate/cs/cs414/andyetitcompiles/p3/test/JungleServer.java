9dfd4725d19f7a386724baed05866c3538fb4914
Brian Larson <Brian Larson@10.0.0.2>
2017-10-20 15:19:34 -0600

    Initial Server test framework.

---
+++ src/test/java/edu/colostate/cs/cs414/andyetitcompiles/p3/test/JungleServer.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package edu.colostate.cs.cs414.andyetitcompiles.p3.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Brian Larson
+ *
+ */
+public class JungleServer {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
