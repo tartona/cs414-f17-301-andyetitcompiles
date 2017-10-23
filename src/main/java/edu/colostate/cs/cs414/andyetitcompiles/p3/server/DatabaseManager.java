643d1d901be4dfa315a1c37a896376e33485d654
Brian Larson <Brian Larson@10.0.0.2>
2017-10-23 15:24:35 -0600

    Started implementing temporary database. connected server functions to temporary database.

--- src/main/java/edu/colostate/cs/cs414/andyetitcompiles/p3/server/DatabaseManager.java
+++ src/main/java/edu/colostate/cs/cs414/andyetitcompiles/p3/server/DatabaseManager.java
@@ -7,8 +7,7 @@ import edu.colostate.cs.cs414.andyetitcompiles.p3.common.User;
 import java.util.HashSet;
 import java.util.Iterator;
 public class DatabaseManager {
-	private User RegisteredUsers;
-	private User OnlineUsers;
+	private Set<User> registeredUsers;
 	private Set<User> users = new HashSet<User>();
 	//TODO add real database
 	
@@ -16,21 +15,29 @@ public class DatabaseManager {
 		
 	}
 
-	public void registerUser(String email, String password, String nickname) 
+	
+	public boolean registerUser(User user) {
+		return registeredUsers.add(user);
+	}
+	public boolean registerUser(String email, String password, String nickname)
 	{
-		RegisteredUsers.setemail(email);
-		RegisteredUsers.setpassword(password);
-		RegisteredUsers.setnickname(nickname);
+		return registerUser(new User(email, password, nickname));
     }
+	
+	public boolean unRegisterUser(User user) {
+		return registeredUsers.remove(user);
+	}
+	
 	public boolean authenticateUser(String email,String password)
 	{
-		if(OnlineUsers.getemail().equals(email)&&OnlineUsers.getpassword().equals(password))
-		{   users.add(OnlineUsers);
-		    return true;
-		}
-		else 
-			return false;
-    
+		for(User user:users) {
+        	if(user.getemail().equalsIgnoreCase(email) || user.getnickname().equalsIgnoreCase(email)) {
+        		if(user.getpassword().equals(password)) {
+        			return true;
+        		}
+        	}
+        }
+		return false;
 	}
 	
 	// take username or email as input for user.
@@ -39,7 +46,7 @@ public class DatabaseManager {
     	//iteration search
         for(Iterator<User> iterator = users.iterator();iterator.hasNext();)
         {  
-        System.out.println(iterator.next());  
+        	System.out.println(iterator.next());  
         }  
     }
 }
--- src/main/java/edu/colostate/cs/cs414/andyetitcompiles/p3/server/JungleServer.java
+++ src/main/java/edu/colostate/cs/cs414/andyetitcompiles/p3/server/JungleServer.java
@@ -30,27 +30,47 @@ public class JungleServer {
 			public void received(Connection c, Object object) {
 				
 				if(object instanceof LoginRequest) {
-					lastReceived = object;
 					LoginRequest loginRequest = (LoginRequest)object;
 					//TODO login, access database, respond to user
+					boolean isSuccessful = false;
+					User user = null;
+					String message = null;
+					c.sendTCP(new LoginResponse(isSuccessful, user, message));
 				}
 				if(object instanceof RegisterRequest) {
 					lastReceived = object;
 					RegisterRequest registerRequest = (RegisterRequest)object;
+					User user = new User(registerRequest.getEmail(), registerRequest.getPassword(), registerRequest.getNickname());
+					boolean isSuccessful = database.registerUser(user);
 					//TODO register, check database for user/email, add to database, respond to user
+					String message = ("User registration " + isSuccessful);
+					c.sendTCP(new RegisterResponse(isSuccessful, message));
 				}
 				if(object instanceof UnregisterRequest) {
 					lastReceived = object;
 					UnregisterRequest unregisterRequest = (UnregisterRequest)object;
 					//TODO unregister, check database for user/email, remove from database, respond to user
+					boolean isSuccessful = false;
+					String message = null;
+					c.sendTCP(new UnregisterResponse(isSuccessful, message));
 				}
 				if(object instanceof UserRequest) {
 					lastReceived = object;
 					UserRequest userRequest = (UserRequest)object;
+					boolean isSuccessful;
+					User user;
+					String message;
+					c.sendTCP(new UserResponse(isSuccessful, user, message));
 				}
 				// Sending invites from the client does not block to wait for a response, so just update the object
 				if(object instanceof InviteRequest) {
 					lastReceived = object;
+					
+					boolean isAccepted;
+					User sender;
+					User recipient;
+					c.sendTCP(new InviteResponse(isAccepted, sender, recipient));
+					
 				}
 			}
 		});	
@@ -67,5 +87,15 @@ public class JungleServer {
 	
 	public Object getLastReceived() {
 		return lastReceived;
+	}
+	
+	//TODO improve email check
+	private boolean validEmail(String email) {
+		if(email.contains("@")) {
+			return email.substring(Math.max(email.length()-6,0)).contains(".");//should be improved
+		}
+		
+		return false;
+		
 	}
 }
