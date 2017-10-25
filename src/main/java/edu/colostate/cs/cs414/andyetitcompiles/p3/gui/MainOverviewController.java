33d5b4817d79ae007636b810fa6d2b9ce8ecb23c
tartona <tartona@gmail.com>
2017-10-25 12:42:23 -0600

    Minor update on GUI

--- src/main/java/edu/colostate/cs/cs414/andyetitcompiles/p3/gui/MainOverviewController.java
+++ src/main/java/edu/colostate/cs/cs414/andyetitcompiles/p3/gui/MainOverviewController.java
@@ -29,37 +29,49 @@ public class MainOverviewController {
 		int numCols = 7 ;
 		int numRows = 9 ;
 
+		// populating Grid Pane
 		for (int i = 0 ; i < numCols ; i++) {
 			ColumnConstraints colConstraints = new ColumnConstraints();
 			colConstraints.setHgrow(Priority.SOMETIMES);
 			gameBoard.getColumnConstraints().add(colConstraints);
 		}
-
 		for (int i = 0 ; i < numRows ; i++) {
 			RowConstraints rowConstraints = new RowConstraints();
 			rowConstraints.setVgrow(Priority.SOMETIMES);
 			gameBoard.getRowConstraints().add(rowConstraints);
 		}
-		for (int j = 0; j < numRows; j++) {
-			for (int i = 0 ; i < numCols ; i++) {
-				addPane(j, i);
-				Label a = new Label();
-				a.setText("He");
-				gameBoard.add((Node)a, i, j);
-			}
-		}
 		for (int i = 0 ; i < numCols ; i++) {
 			for (int j = 0; j < numRows; j++) {
-				Label a = new Label();
-				a.setText("He"); // use set graphic for images
-				gameBoard.add((Node)a, i, j);
+				addPane(j, i);
 			}
 		}
+
+		initBoard(numRows, numCols);
 	}
 
 	@FXML
 	private void openFindUser() {
 		main.showFindUserDialog();
+	}
+
+	private void initBoard(int numRows, int numCols) {
+		for (int j = 0; j < numRows; j++) {
+			for (int i = 0 ; i < numCols ; i++) {
+				Label a = new Label();
+				a.setText(i+","+j); // set graphic for images
+				gameBoard.add((Node)a, i, j);
+			}
+		}
+	}
+
+	private void drawBoard(String[][] board) {
+		int cnt = 0;
+		for (Node node : gameBoard.getChildren()) {
+			if (node instanceof Label) {
+				((Label)node).setText(board[cnt/7][cnt%7]);
+				cnt++;
+			}
+		}
 	}
 
 	private void addPane(int colIndex, int rowIndex) {
@@ -68,11 +80,29 @@ public class MainOverviewController {
 		gameBoard.add(pane, colIndex, rowIndex);
 	}
 
+	int tmprow, tmpcol = 0; // delete later
 	private void cellClicked(MouseEvent e){
+		// testing grid selection
 		Node source = (Node)e.getSource() ;
         Integer colIndex = GridPane.getColumnIndex(source);
         Integer rowIndex = GridPane.getRowIndex(source);
-        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue());
-        cellSelected = true;
+        if(!cellSelected) {
+	        for (Node node : gameBoard.getChildren()) {
+				if (node instanceof Label && gameBoard.getColumnIndex(node) == colIndex.intValue() && gameBoard.getRowIndex(node) == rowIndex.intValue()) {
+					((Label)node).setText("Selected! "+rowIndex.intValue()+","+colIndex.intValue());
+				}
+	        }
+	        tmprow = rowIndex.intValue();
+	        tmpcol = colIndex.intValue();
+	        cellSelected = true;
+        }else {
+        	for (Node node : gameBoard.getChildren()) {
+				if (node instanceof Label && gameBoard.getColumnIndex(node) == tmpcol && gameBoard.getRowIndex(node) == tmprow) {
+					String tmp[] = ((Label)node).getText().split(" ");
+					((Label)node).setText(tmp[1]);
+				}
+	        }
+        	cellSelected = false;
+        }
 	}
 }
